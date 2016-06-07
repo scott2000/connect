c66e9e85932ff19c3a2907de08d0bb2f66f7cbf2
Scott Graydon Taylor <scottipod2000@gmail.com>
2016-06-06 20:46:50 -0500

    Removed Newline

--- Connect/DifficultyViewController.swift
+++ Connect/DifficultyViewController.swift
@@ -11,7 +11,6 @@ import UIKit
 import SpriteKit
 
 class DifficultyViewController: UIViewController {
-    
     @IBOutlet weak var HardButton: Button!
     
     @IBOutlet weak var HighScoreLabel: UILabel!

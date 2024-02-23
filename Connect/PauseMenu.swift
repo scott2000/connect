8d2b62ac5ccfa4222dd30cce2ca3642a55bee7dc
Scott Taylor <scott11x8@gmail.com>
2024-02-22 20:14:47 -0600

    Unstaged changes from 2016

--- Connect/PauseMenu.swift
+++ Connect/PauseMenu.swift
@@ -7,3 +7,23 @@
 //
 
 import Foundation
+import SpriteKit
+import UIKit
+
+class PauseMenu: SKScene {
+    let box: SKShapeNode? = nil
+    let label = SKLabelNode(fontNamed: Grid.font)
+    
+    override func didMoveToView(view: SKView) {
+        backgroundColor = UIColor.whiteColor()
+        let box = SKShapeNode(rectOfSize: CGSize(width: 128, height: 27))
+        box.position = CGPoint(x: size.width/2, y: size.height/2)
+        box.strokeColor = Tile.getColor(.Blue)
+        label.text = "Continue"
+        label.position.y = -8
+        label.fontSize = 16
+        label.fontColor = UIColor.blackColor()
+        box.addChild(label)
+        addChild(box)
+    }
+}
